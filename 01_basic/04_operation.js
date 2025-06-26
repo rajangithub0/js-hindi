18286a62c66667c02169a762897457ce520b775b
rajan <rajan.rana6@gmail.com>
2025-06-27 03:08:20 +0530

    operations

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,57 @@
+
+
+// Arithmetic operations in JavaScript change into negitive number
+let value = 3
+let negVale = -value;
+// console.log(negVale); // -3
+
+// Arithmetic operations
+// console.log(2 + 2); //2
+// console.log(2 - 2); //0
+// console.log(2 * 2); // 4
+// console.log(2 / 2);//
+// console.log(2 ** 2); // 4
+// console.log(2 % 2); // 0
+
+// string concatenation
+// using + operator
+let str1 = "hello"
+let str2 = " rajan";
+let str3 = str1 + str2;
+// console.log(str3); // hello rajan
+
+
+// string concatenation with numbers
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+
+// pre and post  addication with string and number
+// console.log("1" + 3 + 3); // 133
+// console.log(3 + 3 + "1"); // 61
+
+// console.log((3 + 3) * 6 % 1); //
+
+
+// Boolean operations
+// console.log(true); // true
+// console.log(false); // false
+
+// console.log(+""); //
+// console.log(+true); // 1
+// console.log(+false); // 0
+// 
+// 
+
+// assignment conversion
+
+let num1, num2, num3
+num1 = num2 = num3 = 3 + 3;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
+
+++gameCounter;
+console.log(gameCounter); // 102
+
+
