949c2407ddc5cec42b6f529677bf569fa865f688
rajan <rajan.rana6@gmail.com>
2025-07-27 10:47:28 +0530

    string with output

--- 01_basic/08_string.js
+++ 01_basic/08_string.js
@@ -6,39 +6,39 @@ console.log(`hello my name is ${name} and my github repo count is ${repo} .`);
 // string interplusion
 
 const gameName = new String('rajan_HC')
-console.log(gameName);
+console.log(gameName); //[String: 'rajan_HC']
 
-console.log(gameName[0]);
+console.log(gameName[0]); //r
 
-console.log(gameName.__proto__);
+console.log(gameName.__proto__); //{}
 
-console.log(gameName.length);
+console.log(gameName.length); //8
 
-console.log(gameName.toLocaleUpperCase());
+console.log(gameName.toLocaleUpperCase()); //RAJAN_HC
 
-console.log(gameName.charAt(2));
+console.log(gameName.charAt(2)); //j
 
-console.log(gameName.indexOf("j"));
+console.log(gameName.indexOf("j")); //2
 
 
 const newString = gameName.substring(0, 4) // dont allow to give negtive value and end number didnot count
 
-console.log(newString);
+console.log(newString);//raja
 
 const anotherString = gameName.slice(-7, 2) // allowed to give negtive value the nagtive value end of rnd the string
-console.log(anotherString);
+console.log(anotherString); //a
 
 const newStringOne = "      rajan    ";
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne); //      rajan
+console.log(newStringOne.trim()); //rajan
 
 const url = "https://rajan.com/rajan%20about";
 
-console.log(url.replace("%20", "-"));
+console.log(url.replace("%20", "-")); //https://rajan.com/rajan-about
 
-console.log(url.includes("about"))
+console.log(url.includes("about")) //true
 
-console.log(gameName.split('_'));
+console.log(gameName.split('_')); //[ 'rajan', 'HC' ]
 
 
 
