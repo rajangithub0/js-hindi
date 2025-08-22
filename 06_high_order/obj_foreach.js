866f1c6e7b756bcbd3179adbd2c7359cd92cf7f4
rajan <rajan.rana6@gmail.com>
2025-08-22 15:53:44 +0530

    ilteration of for each and object ilteration

---
+++ 06_high_order/obj_foreach.js
@@ -0,0 +1,19 @@
+const myCoding = [
+    {
+        languageName: 'javaScript',
+        languageFileName: "js"
+    },
+    {
+        languageName: 'java',
+        languageFileName: 'java'
+    },
+    {
+
+        languageName: "python",
+        languageFileName: 'py'
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
---
+++ 06_high_order/objectilteration.js
@@ -0,0 +1,28 @@
+// const myObj = {
+//     js: 'JAVASCRIPT',
+//     html: "hyper text markup language",
+//     cpp: "c++",
+//     rb: 'ruby',
+//     swift: "swiftbyapple"
+// }
+
+// for (const key in myObj) {
+//     // console.log(myObj[key]);
+//     console.log(`${key} shortuct id ${myObj[key]}`)
+// }
+
+
+
+// const progamming = ['js', 'rb', 'java', 'js', 'cpp', 'py']
+// for (const key in progamming) {
+//     console.log(progamming[key]);
+
+// }
+
+const map = new Map;
+map.set('IN', 'india');
+map.set("ENG", 'england');
+map.set('FR', 'france')
+// for (const key in map) {
+//     console.log(key);
+// } for in loop not ilteration on map
